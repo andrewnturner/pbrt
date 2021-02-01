dca7847beba73f7b2625f54c4cec5a117601ec58
andrewnturner <andrewturner397@gmail.com>
2021-02-01 23:07:36 +0000

    Auxiliary methods for vectors

--- pbrt/src/core/geometry/vector.rs
+++ pbrt/src/core/geometry/vector.rs
@@ -1,12 +1,23 @@
 use std::ops::{Add, AddAssign, Sub, SubAssign,  Mul, MulAssign, Div, DivAssign, Neg};
+use std::cmp::PartialOrd;
 
 use num_traits::{Num as NumBase, Float, Signed};
 
-pub trait Num: NumBase + Copy {
+#[inline]
+fn min<T: Num>(a: T, b: T) -> T {
+    if a < b { a } else { b }
+}
+
+#[inline]
+fn max<T: Num>(a: T, b: T) -> T {
+    if a > b { a } else { b }
+}
+
+pub trait Num: NumBase + PartialOrd + Copy {
     fn is_nan(&self) -> bool;
 }
 
-impl<T: NumBase + Copy> Num for T {
+impl<T: NumBase + PartialOrd + Copy> Num for T {
     #[inline]
     fn is_nan(&self) -> bool {
         self != self
@@ -43,14 +54,80 @@ impl<T: Num> Vector2<T> {
     pub fn has_nans(&self) -> bool {
         self.x.is_nan() || self.y.is_nan()
     }
+
+    pub fn dot(&self, other: &Self) -> T {
+        (self.x * other.x) + (self.y * other.y)
+    }
+
+    pub fn length_squared(&self) -> T {
+        (self.x * self.x) + (self.y * self.y)
+    }
+
+    pub fn min_component(&self) -> T {
+        min(self.x, self.y)
+    }
+
+    pub fn max_component(&self) -> T {
+        max(self.x, self.y)
+    }
+
+    pub fn min_dimension(&self) -> usize {
+        if self.x < self.y {
+            0
+        } else {
+            1
+        }
+    }
+
+    pub fn max_dimension(&self) -> usize {
+        if self.x > self.y {
+            0
+        } else {
+            1
+        }
+    }
+
+    pub fn min(&self, other: &Self) -> Self {
+        Self {
+            x: min(self.x, other.x),
+            y: min(self.y, other.y),
+        }
+    }
+
+    pub fn max(&self, other: &Self) -> Self {
+        Self {
+            x: max(self.x, other.x),
+            y: max(self.y, other.y),
+        }
+    }
+
+    pub fn index(&self, i: usize) -> T {
+        match i {
+            0 => self.x,
+            1 => self.y,
+            _ => panic!("Invalid index"),
+        }
+    }
+
+    pub fn permute(&self, x_index: usize, y_index: usize) -> Self {
+        Self {
+            x: self.index(x_index),
+            y: self.index(y_index),
+        }
+    }
 }
 
-impl<T: Signed + Copy> Vector2<T> {
+impl<T: Num + Signed> Vector2<T> {
     pub fn abs(&self) -> Self {
         Self {
             x: self.x.abs(),
             y: self.y.abs(),
         }
+    }
+
+    #[inline]
+    pub fn abs_dot(self, other: Self) -> T {
+        self.dot(&other).abs()
     }
 }
 
@@ -118,7 +195,7 @@ impl<T: Float> DivAssign<T> for Vector2<T> {
     }
 }
 
-impl<T: Signed + Copy> Neg for Vector2<T> {
+impl<T: Num + Signed> Neg for Vector2<T> {
     type Output = Self;
 
     fn neg(self) -> Self {
@@ -126,6 +203,18 @@ impl<T: Signed + Copy> Neg for Vector2<T> {
             x: -self.x,
             y: -self.y,
         }
+    }
+}
+
+impl<T> Vector2<T> where T: Float, Vector2<T>: Div<T> {
+    pub fn length(&self) -> T {
+        self.length_squared().sqrt()
+    }
+
+    pub fn normalise(self) -> <Vector2<T> as Div<T>>::Output {
+        let l = self.length();
+
+        self / l
     }
 }
 
@@ -141,15 +230,138 @@ impl<T: Num> Vector3<T> {
     pub fn has_nans(&self) -> bool {
         self.x.is_nan() || self.y.is_nan() || self.z.is_nan()
     }
+
+    pub fn dot(&self, other: &Self) -> T {
+        (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Self {
+            x: (self.y * other.z) - (self.z * other.y),
+            y: (self.z * other.x) - (self.x * other.z),
+            z: (self.x * other.y) - (self.y * other.x),
+        }
+    }
+
+    pub fn length_squared(&self) -> T {
+        (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
+    }
+
+    pub fn min_component(&self) -> T {
+        min(self.x, min(self.y, self.z))
+    }
+
+    pub fn max_component(&self) -> T {
+        max(self.x, max(self.y, self.z))
+    }
+
+    pub fn min_dimension(&self) -> usize {
+        if self.x < self.y {
+            if self.x < self.z {
+                0
+            } else {
+                2
+            }
+        } else {
+            if self.y < self.z {
+                1
+            } else {
+                2
+            }
+        }
+    }
+
+    pub fn max_dimension(&self) -> usize {
+        if self.x > self.y {
+            if self.x > self.z {
+                0
+            } else {
+                2
+            }
+        } else {
+            if self.y > self.z {
+                1
+            } else {
+                2
+            }
+        }
+    }
+
+    pub fn min(&self, other: &Self) -> Self {
+        Self {
+            x: min(self.x, other.x),
+            y: min(self.y, other.y),
+            z: min(self.z, other.z),
+        }
+    }
+
+    pub fn max(&self, other: &Self) -> Self {
+        Self {
+            x: max(self.x, other.x),
+            y: max(self.y, other.y),
+            z: max(self.z, other.z),
+        }
+    }
+
+    pub fn index(&self, i: usize) -> T {
+        match i {
+            0 => self.x,
+            1 => self.y,
+            2 => self.z,
+            _ => panic!("Invalid index"),
+        }
+    }
+
+    pub fn permute(&self, x_index: usize, y_index: usize, z_index: usize) -> Self {
+        Self {
+            x: self.index(x_index),
+            y: self.index(y_index),
+            z: self.index(z_index),
+        }
+    }
 }
 
-impl<T: Signed + Copy> Vector3<T> {
+impl<T: Num + Signed> Vector3<T> {
     pub fn abs(&self) -> Self {
         Self {
             x: self.x.abs(),
             y: self.y.abs(),
             z: self.z.abs(),
         }
+    }
+
+        #[inline]
+    pub fn abs_dot(self, other: Self) -> T {
+        self.dot(&other).abs()
+    }
+}
+
+impl<T: Float> Vector3<T> where T: Float, Vector3<T>: Div<T, Output=Vector3<T>> {
+    pub fn length(&self) -> T {
+        self.length_squared().sqrt()
+    }
+
+    pub fn normalise(self) -> <Vector3<T> as Div<T>>::Output {
+        let l = self.length();
+
+        self / l
+    }
+
+    pub fn coordinate_system(&self) -> (Self, Self) {
+        debug_assert!((self.length_squared() - T::one()).abs() < T::epsilon());
+
+        let w: Self = if self.x.abs() > self.y.abs() {
+            let a: Self = Self { x: -self.z, y: T::zero(), z: self.x };
+            let b: T = ((self.x * self.x) + (self.z * self.z)).sqrt();
+                a / b
+        } else {
+            Self { x: T::zero(), y: self.z, z: -self.y }
+                / ((self.y * self.y) + (self.z * self.z)).sqrt()
+        };
+
+        let u = self.cross(&w);
+
+        (w, u)
     }
 }
 
@@ -222,7 +434,7 @@ impl<T: Float> DivAssign<T> for Vector3<T> {
         self.z = self.z * inv;
     }
 }
-impl<T: Signed + Copy> Neg for Vector3<T> {
+impl<T: Num + Signed> Neg for Vector3<T> {
     type Output = Self;
 
     fn neg(self) -> Self {
@@ -237,6 +449,12 @@ impl<T: Signed + Copy> Neg for Vector3<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    macro_rules! assert_delta {
+        ($x:expr, $y:expr, $d:expr) => {
+            if !($x - $y < $d || $y - $x < $d) { panic!(); }
+        }
+    }
 
     #[test]
     fn make_zero_vector2() {
@@ -432,5 +650,217 @@ mod tests {
         let v = Vector3f { x: 1.0, y: -2.0, z: 3.0 };
 
         assert_eq!(v.abs(), Vector3f { x: 1.0, y: 2.0, z: 3.0 });
+    }
+
+    #[test]
+    fn dot_vector2() {
+        let v = Vector2i { x: -1, y: -2 };
+        let w = Vector2i { x: 3, y: 4 };
+
+        assert_eq!(v.dot(&w), -11);
+    }
+
+    #[test]
+    fn dot_vector3() {
+        let v = Vector3f { x: -1.0, y: -2.0, z: -3.0 };
+        let w = Vector3f { x: 3.0, y: 4.0, z: 5.0 };
+
+        assert_eq!(v.dot(&w), -26.0);
+    }
+
+    #[test]
+    fn abs_dot_vector2() {
+        let v = Vector2i { x: -1, y: -2 };
+        let w = Vector2i { x: 3, y: 4 };
+
+        assert_eq!(v.abs_dot(w), 11);
+    }
+
+    #[test]
+    fn abs_dot_vector3() {
+        let v = Vector3f { x: -1.0, y: -2.0, z: -3.0 };
+        let w = Vector3f { x: 3.0, y: 4.0, z: 5.0 };
+
+        assert_eq!(v.abs_dot(w), 26.0);
+    }
+
+    #[test]
+    fn cross_vector3() {
+        let v = Vector3f { x: -1.0, y: 2.0, z: -3.0 };
+        let w = Vector3f { x: 4.0, y: 5.0, z: 6.0 };
+
+        assert_eq!(v.cross(&w), Vector3f { x: 27.0, y: -6.0, z: -13.0 });
+    }
+
+    #[test]
+    fn length_squared_vector2() {
+        let v = Vector2i { x: 3, y: -4 };
+
+        assert_eq!(v.length_squared(), 25);
+    }
+
+    #[test]
+    fn length_vector2() {
+        let v = Vector2f { x: 3.0, y: -4.0 };
+
+        assert_eq!(v.length(), 5.0);
+    }
+
+    #[test]
+    fn length_squared_vector3() {
+        let v = Vector3f { x: 2.0, y: -10.0, z: 11.0 };
+
+        assert_eq!(v.length_squared(), 225.0);
+    }
+
+    #[test]
+    fn length_vector3() {
+        let v = Vector3f { x: 2.0, y: -10.0, z: 11.0 };
+
+        assert_eq!(v.length(), 15.0);
+    }
+
+    #[test]
+    fn normalise_vector2() {
+        let v = Vector2f { x: 2.0, y: 0.0 };
+
+        assert_eq!(v.normalise(), Vector2f { x: 1.0, y: 0.0 });
+    }
+
+    #[test]
+    fn normalise_vector3() {
+        let v = Vector3f { x: 1.0, y: 1.0, z: 1.0 };
+
+        let n = 1.0 / (3.0).sqrt();
+        assert_eq!(v.normalise(), Vector3f { x: n, y: n, z: n });
+    }
+
+    #[test]
+    fn min_component_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.min_component(), 1);
+    }
+
+    #[test]
+    fn max_component_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.max_component(), 2);
+    }
+
+    #[test]
+    fn min_component_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.min_component(), 1.0);
+    }
+
+    #[test]
+    fn max_component_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.max_component(), 3.0);
+    }
+
+    #[test]
+    fn min_dimension_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.min_dimension(), 0);
+    }
+
+    #[test]
+    fn max_dimension_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.max_dimension(), 1);
+    }
+
+    #[test]
+    fn min_dimension_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.min_dimension(), 0);
+    }
+
+    #[test]
+    fn max_dimension_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.max_dimension(), 2);
+    }
+
+    #[test]
+    fn min_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+        let w = Vector2i { x: 3, y: 0 };
+
+        assert_eq!(v.min(&w), Vector2i { x: 1, y: 0 });
+    }
+
+    #[test]
+    fn max_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+        let w = Vector2i { x: 3, y: 0 };
+
+        assert_eq!(v.max(&w), Vector2i { x: 3, y: 2 });
+    }
+
+    #[test]
+    fn min_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+        let w = Vector3f { x: 3.0, y: 0.0, z: 1.0 };
+
+        assert_eq!(v.min(&w), Vector3f { x: 1.0, y: 0.0, z: 1.0 });
+    }
+
+    #[test]
+    fn max_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+        let w = Vector3f { x: 3.0, y: 0.0, z: 1.0 };
+
+        assert_eq!(v.max(&w), Vector3f { x: 3.0, y: 2.0, z: 3.0 });
+    }
+
+    #[test]
+    fn index_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.index(1), 2);
+    }
+
+    #[test]
+    fn permute_vector2() {
+        let v = Vector2i { x: 1, y: 2 };
+
+        assert_eq!(v.permute(1, 0), Vector2i { x: 2, y: 1 });
+    }
+
+    #[test]
+    fn index_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.index(1), 2.0);
+    }
+
+    #[test]
+    fn permute_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert_eq!(v.permute(1, 2, 0), Vector3f { x: 2.0, y: 3.0, z: 1.0 });
+    }
+
+    #[test]
+    fn coordinate_system_vector3() {
+        let v = Vector3f { x: 1.0, y: 2.0, z: 3.0 }
+            .normalise();
+
+        let (w, u) = v.coordinate_system();
+
+        assert_delta!(w.length_squared(), 1.0, 0.000001);
+        assert_delta!(u.length_squared(), 1.0, 0.000001);
+        assert_delta!(v.dot(&w), 0.0, 0.000001);
+        assert_delta!(v.dot(&u), 0.0, 0.000001);
     }
 }
